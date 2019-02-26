1698324126abdf3cd674c2313ba97ac4d9efd274
Mac Pro 3 <macpro3@mwnewmacpro2.wifi.bcit.ca>
2019-02-26 15:11:17 -0800

    Initial Commit

---
+++ TaskerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskerApp
+//
+//  Created by Mac Pro 3 on 2019-02-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
